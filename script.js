9d71bcd925f3d278fab04d99d62b7aca79755e28
Imrul Kayes <kayeskayes65@gmail.com>
2025-05-26 21:30:43 +0600

    Fix responsive issue in navbar

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// script.js
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.querySelector("header nav");
 
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
