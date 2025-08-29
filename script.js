c54b68adb37622a67c35459f6173ca1fcb16fc94
Imrul Kayes <kayeakayes65@gmail.com>
2025-08-29 22:00:16 +0600

    updated menu bar

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // script.js
+// const menuToggle = document.getElementById("menuToggle");
+// const nav = document.querySelector("header nav");
+
+// menuToggle.addEventListener("click", () => {
+//   nav.classList.toggle("active");
+// });
+
 const menuToggle = document.getElementById("menuToggle");
 const nav = document.querySelector("header nav");
+const navLinks = document.querySelectorAll("header nav a");
 
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 });
