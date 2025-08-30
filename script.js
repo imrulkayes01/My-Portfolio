c8afbc06b34c8f44a197d6d6dc1442df66c54cfa
Imrul Kayes <kayeakayes65@gmail.com>
2025-08-30 11:05:05 +0600

    form update

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 // script.js
-// const menuToggle = document.getElementById("menuToggle");
-// const nav = document.querySelector("header nav");
-
-// menuToggle.addEventListener("click", () => {
-//   nav.classList.toggle("active");
-// });
-
 const menuToggle = document.getElementById("menuToggle");
 const nav = document.querySelector("header nav");
 const navLinks = document.querySelectorAll("header nav a");
